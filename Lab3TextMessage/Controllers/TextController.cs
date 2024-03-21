d067ab624061840b94575492075f7448b90d7e0e
adonegan <donegan.aileen@gmail.com>
2024-03-21 21:53:39 +0000

    aileens version

---
+++ Lab3TextMessage/Controllers/TextController.cs
@@ -0,0 +1,37 @@
+﻿using Lab3TextMessage.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3TextMessage.Controllers
+{
+    public class TextController : Controller
+    {
+        TextMessage Text1 = new TextMessage() {DestinationPhone="5555555555", TextContent=" " }; 
+        
+        public ActionResult Display()
+        {
+            return View(Text1);
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View(Text1);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TextMessage _Text)
+        {
+            //Text1.TextContent = _Text.TextContent;
+            return RedirectToAction("Success");
+        }
+
+        public ActionResult Success() 
+        {
+            ViewBag.SuccessMessage = "Success! You've sent your text!";
+            return View();
+        }
+
+        
+    }
+}
---
+++ Lab3TextMessage/Models/TextMessage.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab3TextMessage.Models
+{
+    public class TextMessage
+    {
+        [Required]
+        [Display(Name = "Phone Number:")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+        public string DestinationPhone { get; set; }
+
+        [MaxLength(140)]
+        [Display(Name = "Content")]
+        public string TextContent { get; set; }
+    }
+
+}
